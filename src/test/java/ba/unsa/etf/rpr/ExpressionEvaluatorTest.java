c60f6031086fa637638717723eb221d76a78d429
selmaalj <sljuhar1@etf.unsa.ba>
2022-11-18 00:28:34 +0100

    Testovi

---
+++ src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
@@ -0,0 +1,46 @@
+package ba.unsa.etf.rpr;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExpressionEvaluatorTest {
+    @Test
+    void testIzuzetka() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            ExpressionEvaluator.evaluate("( 5 )");
+        });
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            ExpressionEvaluator.evaluate("( 1 + 2 + 3 )");
+        });
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            ExpressionEvaluator.evaluate("( )");
+        });
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            ExpressionEvaluator.evaluate(" ");
+        });
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            ExpressionEvaluator.evaluate("( ( 2 + 2 ) ");
+        });
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            ExpressionEvaluator.evaluate(" 1 + ( 2 * 3 )   ");
+        });
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            ExpressionEvaluator.evaluate(" 1 + ( 2 + 4 + 6 )  ");
+        });
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            ExpressionEvaluator.evaluate(" ( 1 / ( 4 * 7 )))  ");
+        });
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            ExpressionEvaluator.evaluate(" ( 7 / 0 ) ");
+        });
+    }
+    @Test
+    void testEvaluate(){
+        Assertions.assertEquals(14.0, ExpressionEvaluator.evaluate(" ( 2 * ( 3 + 4 ) )"));
+        Assertions.assertEquals(5.0, ExpressionEvaluator.evaluate(" ( ( sqrt 100 ) / 2 )"));
+        Assertions.assertEquals(18.0, ExpressionEvaluator.evaluate("( ( ( sqrt 36 ) / 2 ) * 6 )"));
+        Assertions.assertEquals(3.6, ExpressionEvaluator.evaluate(" 3.6 "));
+        Assertions.assertEquals(0.0, ExpressionEvaluator.evaluate(" ( 5.7 * 0 ) "));
+        Assertions.assertEquals(3.75, ExpressionEvaluator.evaluate("( ( 1 + ( ( ( sqrt ( 9 * ( 2 - 1 ) ) ) / 1.5 ) / 8 ) ) * 3 )"));
+    }
+}
